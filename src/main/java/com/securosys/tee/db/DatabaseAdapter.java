4335601192261aede23ddd1ff703bb7bd18a441c
GitLab Runner <gitlab-runner@deb11buildxl.securosys.ch>
2025-09-08 13:42:23 +0200

    Release v.1.0.0

---
+++ src/main/java/com/securosys/tee/db/DatabaseAdapter.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c)2025 Securosys SA, authors: Tomasz Madej
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ **/
+package com.securosys.tee.db;
+
+import com.securosys.tee.dto.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface DatabaseAdapter {
+    void saveState(String key, String value);
+    String readState(String key, String defaultValue);
+    void deleteState(String key);
+}
